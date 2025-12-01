619ab228f19d9d7aa7fe21b9e4305a3b99e52655
Dextrobyte <dextrobyte@gmail.com>
2025-12-01 18:40:18 +0530

    final fix for broken key script

--- generate-mok-key.sh
+++ generate-mok-key.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Generate an unencrypted 2048-bit private key
-openssl genrsa -out mok.key 2048
-
 # Generate a self-signed X.509 certificate
+
 openssl req -new -x509 -newkey rsa:2048 -nodes \
   -keyout MOK.key -outform DER -out MOK.der \
   -days 36500 -subj "/CN= Linuwu-Sense Signing Key/"
